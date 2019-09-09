b6bac2b84593bcbd1b36041e7a404d9440eef705
Strating, Michael H <michael.strating@dematic.com>
2019-09-09 09:04:39 -0400

    WIP monster models. See TODO comments in file

--- app/api/models.py
+++ app/api/models.py
@@ -2,31 +2,15 @@ from django.db import models
 
 
 class Monster(models.Model):
-    """
-    This current outline is based on the json structure found
-    here: https://gist.github.com/tkfu/9819e4ac6d529e225e9fc58b358c3479
-    I would like to determine what the best strategy would be for
-    storing this data in the future.
-    """
-    name = models.CharField(max_length=50)
-    armor_class = models.IntegerField()
-    hit_points = models.CharField(max_length=25)  # TODO: Better way? EX] hit_points = '135 (18d10 + 36)'
-    speed = models.CharField(max_length=50)  # TODO: Better way? EX] speed = '30 ft., swim 40 ft.'
-    # TODO: Determine if the stats can be stored in a better way
+    name = models.CharField(max_length=50)  # Monster name
+    armor_class = models.ForeignKey('ArmorClass', on_delete=models.CASCADE, related_name='monsters')
+    hit_points = models.CharField(max_length=25)
     STR = models.IntegerField()
     DEX = models.IntegerField()
     CON = models.IntegerField()
     INT = models.IntegerField()
     WIS = models.IntegerField()
     CHA = models.IntegerField()
-    STR_mod = models.IntegerField()
-    DEX_mod = models.IntegerField()
-    CON_mod = models.IntegerField()
-    INT_mod = models.IntegerField()
-    WIS_mod = models.IntegerField()
-    CHA_mod = models.IntegerField()
-    saving_throws = models.CharField(max_length=50)  # TODO: Better way? EX] CON +6, STR +5, ...
-    skills = models.CharField(max_length=100)  # TODO: Better way? EX] perception +3, history +2, ...
     senses = models.CharField(max_length=100)  # TODO: Better way? EX] Darkvision 120 ft.,  Passive Perception 20, ...
     languages = models.CharField(max_length=100)  # TODO: Better way? EX] Deep Speech, Telepathy 120 ft., ...
     challenge = models.IntegerField()
@@ -37,3 +21,61 @@ class Monster(models.Model):
     img_url = models.CharField(max_length=255)  # TODO: Better way of storing a url?
 
 
+class ArmorClass(models.Model):
+    """
+    Armor Class for monsters
+    Example: { value: 16, note: 'Natural Armor' }
+    """
+    value = models.IntegerField()   # AC value
+    note = models.CharField(max_length=100) # Any note or explaination for AC
+
+
+class MovementTypes(models.Model):
+    """
+    Lookup table for movement types in Dungeons and Dragons
+    Example: { name: 'Swimming' }
+    """
+    name = models.CharField(max_length=25)  # Name of movement type
+
+
+class MonsterMovementSpeed(models.Model):
+    """
+    Monster movement speeds for each movement type required
+    Example: { monster: 'Angry Fish', movement_type: 'Swimming', value: 35 }
+    """
+    monster = models.ForeignKey('Monster', on_delete=models.CASCADE, related_name='speeds')
+    movement_type = models.ForeignKey('MovementType', on_delete=models.CASCADE, related_name='speeds')
+    value = models.IntegerField()   # The speed in ft. that the monster moves
+
+
+class AbilityScore(models.Model):
+    """
+    Lookup table for ability scores in Dungeons and Dragons
+    Example: { name: 'Constitution' }
+    """
+    name = models.CharField(max_length=15)  # Name of the ability score
+
+
+class MonsterSavingThrow(models.Model):
+    """
+    Monster saving throws and their associated modifiers
+    Example: { monster: 'Angry Fish', ability_score: 'Constitution', value: -1 }
+    """
+    monster = models.ForeignKey('Monster', on_delete=models.CASCADE, related_name='saving_throws')
+    ability_score = models.ForeignKey('AbilityScore', on_delete=models.CASCADE, related_name='saving_throws')
+    value = models.IntegerField()   # The roll modifier value for monster saving throws
+
+
+class Skill(models.Model):
+    """
+    Lookup table for skills in Dungeons and Dragons
+    """
+    name = models.CharField(max_length=25)
+
+
+class MonsterSkill(models.Model):
+    monster = models.ForeignKey('Monster', on_delete=models.CASCADE, related_name='skills')
+    skill = models.ForeignKey('Skill', on_delete=models.CASCADE, related_name='skills')
+    value = models.IntegerField()   # The roll modifier value for monster skills
+
+# TODO: BUILD OUT THE MODELS FOR SENSES AND LANGUAGES!!
