24ce7b53f75a8fcbcbd54125cb1e8c2b482f952c
Strating, Michael H <michael.strating@dematic.com>
2019-09-10 14:55:33 -0400

    Added models to admin screen

--- app/api/admin.py
+++ app/api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+import api.models as mdl
+
+
+admin.site.register(mdl.Monster)
+admin.site.register(mdl.ArmorClass)
+admin.site.register(mdl.MovementType)
+admin.site.register(mdl.MonsterMovementSpeed)
+admin.site.register(mdl.AbilityScore)
+admin.site.register(mdl.MonsterSavingThrow)
+admin.site.register(mdl.Skill)
+admin.site.register(mdl.MonsterSkill)
+admin.site.register(mdl.Sense)
+admin.site.register(mdl.MonsterSense)
+admin.site.register(mdl.Language)
+admin.site.register(mdl.MonsterLanguage)
