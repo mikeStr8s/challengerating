4f397b43ae795b37f121a5edf22c29381f1116fa
Strating, Michael H <michael.strating@dematic.com>
2019-09-17 14:58:50 -0400

    fully implement admin page for Monster objects

--- app/api/admin.py
+++ app/api/admin.py
@@ -3,7 +3,79 @@ from django.contrib import admin
 import api.models as mdl
 
 
-admin.site.register(mdl.Monster)
+class MonsterMovementSpeedInline(admin.TabularInline):
+    model = mdl.MonsterMovementSpeed
+    extra = 1
+
+
+class MonsterDamageImmunityInline(admin.TabularInline):
+    model = mdl.MonsterDamageImmunity
+    extra = 1
+
+
+class MonsterDamageResistanceInline(admin.TabularInline):
+    model = mdl.MonsterDamageResistance
+    extra = 1
+
+
+class MonsterDamageVulnerabilityInline(admin.TabularInline):
+    model = mdl.MonsterDamageVulnerability
+    extra = 1
+
+
+class MonsterSavingThrowInline(admin.TabularInline):
+    model = mdl.MonsterSavingThrow
+    extra = 1
+
+
+class MonsterSkillInline(admin.TabularInline):
+    model = mdl.MonsterSkill
+    extra = 1
+
+
+class MonsterSenseInline(admin.TabularInline):
+    model = mdl.MonsterSense
+    extra = 1
+
+
+class MonsterLanguageInline(admin.TabularInline):
+    model = mdl.MonsterLanguage
+    extra = 1
+
+
+class MonsterAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('General Information', {
+            'fields': (('name', 'armor_class', 'hit_points'), ('challenge', 'experience'), ('STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA'), 'condition_immunities')
+        }),
+        ('Actions', {
+            'classes': ('collapse',),
+            'fields': ('actions',)
+        }),
+        ('Reactions', {
+            'classes': ('collapse',),
+            'fields': ('reactions',)
+        }),
+        ('Traits', {
+            'classes': ('collapse',),
+            'fields': ('traits',)
+        }),
+        ('Legendary Actions', {
+            'classes': ('collapse',),
+            'fields': ('legendary_actions',)
+        }),
+    )
+    inlines = [
+        MonsterMovementSpeedInline,
+        MonsterSavingThrowInline,
+        MonsterSkillInline,
+        MonsterSenseInline,
+        MonsterDamageImmunityInline,
+        MonsterDamageResistanceInline,
+        MonsterDamageVulnerabilityInline,
+    ]
+
+admin.site.register(mdl.Monster, MonsterAdmin)
 admin.site.register(mdl.ArmorClass)
 admin.site.register(mdl.MovementType)
 admin.site.register(mdl.MonsterMovementSpeed)
