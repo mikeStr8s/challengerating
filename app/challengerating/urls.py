404336019a29efb3ea44be302f35e809745ad530
Strating, Michael H <michael.strating@dematic.com>
2019-09-20 13:37:46 -0400

    Added encounter to the core

--- app/challengerating/urls.py
+++ app/challengerating/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api_v1/', include('api.urls')),
+    path('encounter/', include('encounter.urls')),
 ]
