ce19420d1743b3bcfba4ba6b863b61bca9634cd9
Strating, Michael H <michael.strating@dematic.com>
2019-10-03 12:27:49 -0400

    corrected some data movement and added docstring

--- app/encounter/static/encounter/index.js
+++ app/encounter/static/encounter/index.js
@@ -8,14 +8,20 @@ const loadPage = (endpoints) => {
  * @param {Object} handler - The function handler for the returned api data
  */
 const getApiEndpoint = (URL, handler) => {
+    let data;
     $.ajax({
         url: URL,
-        success: (result) => handler(result)
+        success: (result) => data = handler(result)
     });
+    return data;
 }
 
+/**
+ * Handler and cleanser for the list of monster data to be returned wholesale to the calling function for consumption.
+ * @param {Array} data - List of initial monster objects, {name: <string:monster_name>, url: "/api_v1/monsters/<int:id>"}, to be used for traversal.
+ */
 const getMonsterList = (data) => {
-    console.log(data);
+    return data;
 }
 
 const returnMonsterList = (data) => {
